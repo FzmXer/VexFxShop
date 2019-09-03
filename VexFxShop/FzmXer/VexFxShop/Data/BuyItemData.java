a3e723d13b492609885fcd5e5d294eb36ecc9433
FzmXer <52704583+FzmXer@users.noreply.github.com>
2019-09-03 20:55:05 +0800

    Create BuyItemData.java

---
+++ VexFxShop/FzmXer/VexFxShop/Data/BuyItemData.java
@@ -0,0 +1,460 @@
+package FzmXer.VexFxShop.Data;
+
+public class BuyItemData {
+	
+	private String bg_image;
+	private int bg_x;
+	private int bg_y;
+	private int bg_w;
+	private int bg_h;
+	private int bg_xs;
+	private int bg_ys;
+	
+	private String itemstack_image;
+	private int itemstack_h;
+	
+	private String slot_image;
+	private int slot_image_x;
+	private int slot_image_y;
+	private int slot_image_w;
+	private int slot_image_h;
+	
+	private int slot_x;
+	private int slot_y;
+	
+	private String name_text;
+	private int name_x;
+	private int name_y;
+	private double name_fontsize;
+	
+	private String number_text;
+	private int number_x;
+	private int number_y;
+	private double number_fontsize;
+	
+	private String money_text;
+	private String money_text_suffix;
+	private int money_x;
+	private int money_y;
+	private double money_fontsize;
+	
+	private String point_text;
+	private String point_text_suffix;
+	private int point_x;
+	private int point_y;
+	private double point_fontsize;
+	
+	private String buytext_text;
+	private int buytext_x;
+	private int buytext_y;
+	private double buytext_fontsize;
+	
+	private int input_x;
+	private int input_y;
+	private int input_w;
+	private int input_h;
+	
+	private String back_text;
+	private String back_image1;
+	private String back_image2;
+	private int back_x;
+	private int back_y;
+	private int back_w;
+	private int back_h;
+	
+	private String down_text;
+	private String down_image1;
+	private String down_image2;
+	private int down_x;
+	private int down_y;
+	private int down_w;
+	private int down_h;
+	
+	private String buy_text;
+	private String buy_image1;
+	private String buy_image2;
+	private int buy_x;
+	private int buy_y;
+	private int buy_w;
+	private int buy_h;
+	
+	public double getName_fontsize() {
+		return name_fontsize;
+	}
+	public void setName_fontsize(double name_fontsize) {
+		this.name_fontsize = name_fontsize;
+	}
+	public String getBg_image() {
+		return bg_image;
+	}
+	public void setBg_image(String bg_image) {
+		this.bg_image = bg_image;
+	}
+	public int getBg_x() {
+		return bg_x;
+	}
+	public void setBg_x(int bg_x) {
+		this.bg_x = bg_x;
+	}
+	public int getBg_y() {
+		return bg_y;
+	}
+	public void setBg_y(int bg_y) {
+		this.bg_y = bg_y;
+	}
+	public int getBg_w() {
+		return bg_w;
+	}
+	public void setBg_w(int bg_w) {
+		this.bg_w = bg_w;
+	}
+	public int getBg_h() {
+		return bg_h;
+	}
+	public void setBg_h(int bg_h) {
+		this.bg_h = bg_h;
+	}
+	public int getBg_xs() {
+		return bg_xs;
+	}
+	public void setBg_xs(int bg_xs) {
+		this.bg_xs = bg_xs;
+	}
+	public int getBg_ys() {
+		return bg_ys;
+	}
+	public void setBg_ys(int bg_ys) {
+		this.bg_ys = bg_ys;
+	}
+	public String getItemstack_image() {
+		return itemstack_image;
+	}
+	public void setItemstack_image(String itemstack_image) {
+		this.itemstack_image = itemstack_image;
+	}
+	public int getItemstack_h() {
+		return itemstack_h;
+	}
+	public void setItemstack_h(int itemstack_h) {
+		this.itemstack_h = itemstack_h;
+	}
+	public String getSlot_image() {
+		return slot_image;
+	}
+	public void setSlot_image(String slot_image) {
+		this.slot_image = slot_image;
+	}
+	public int getSlot_image_x() {
+		return slot_image_x;
+	}
+	public void setSlot_image_x(int slot_image_x) {
+		this.slot_image_x = slot_image_x;
+	}
+	public int getSlot_image_y() {
+		return slot_image_y;
+	}
+	public void setSlot_image_y(int slot_image_y) {
+		this.slot_image_y = slot_image_y;
+	}
+	public int getSlot_image_w() {
+		return slot_image_w;
+	}
+	public void setSlot_image_w(int slot_image_w) {
+		this.slot_image_w = slot_image_w;
+	}
+	public int getSlot_image_h() {
+		return slot_image_h;
+	}
+	public void setSlot_image_h(int slot_image_h) {
+		this.slot_image_h = slot_image_h;
+	}
+	public int getSlot_x() {
+		return slot_x;
+	}
+	public void setSlot_x(int slot_x) {
+		this.slot_x = slot_x;
+	}
+	public int getSlot_y() {
+		return slot_y;
+	}
+	public void setSlot_y(int slot_y) {
+		this.slot_y = slot_y;
+	}
+	public String getName_text() {
+		return name_text;
+	}
+	public void setName_text(String name_text) {
+		this.name_text = name_text;
+	}
+	public int getName_x() {
+		return name_x;
+	}
+	public void setName_x(int name_x) {
+		this.name_x = name_x;
+	}
+	public int getName_y() {
+		return name_y;
+	}
+	public void setName_y(int name_y) {
+		this.name_y = name_y;
+	}
+	public String getNumber_text() {
+		return number_text;
+	}
+	public void setNumber_text(String number_text) {
+		this.number_text = number_text;
+	}
+	public int getNumber_x() {
+		return number_x;
+	}
+	public void setNumber_x(int number_x) {
+		this.number_x = number_x;
+	}
+	public int getNumber_y() {
+		return number_y;
+	}
+	public void setNumber_y(int number_y) {
+		this.number_y = number_y;
+	}
+	public double getNumber_fontsize() {
+		return number_fontsize;
+	}
+	public void setNumber_fontsize(double number_fontsize) {
+		this.number_fontsize = number_fontsize;
+	}
+	public String getMoney_text() {
+		return money_text;
+	}
+	public void setMoney_text(String money_text) {
+		this.money_text = money_text;
+	}
+	public String getMoney_text_suffix() {
+		return money_text_suffix;
+	}
+	public void setMoney_text_suffix(String money_text_suffix) {
+		this.money_text_suffix = money_text_suffix;
+	}
+	public int getMoney_x() {
+		return money_x;
+	}
+	public void setMoney_x(int money_x) {
+		this.money_x = money_x;
+	}
+	public int getMoney_y() {
+		return money_y;
+	}
+	public void setMoney_y(int money_y) {
+		this.money_y = money_y;
+	}
+	public double getMoney_fontsize() {
+		return money_fontsize;
+	}
+	public void setMoney_fontsize(double money_fontsize) {
+		this.money_fontsize = money_fontsize;
+	}
+	public String getPoint_text() {
+		return point_text;
+	}
+	public void setPoint_text(String point_text) {
+		this.point_text = point_text;
+	}
+	public String getPoint_text_suffix() {
+		return point_text_suffix;
+	}
+	public void setPoint_text_suffix(String point_text_suffix) {
+		this.point_text_suffix = point_text_suffix;
+	}
+	public int getPoint_x() {
+		return point_x;
+	}
+	public void setPoint_x(int point_x) {
+		this.point_x = point_x;
+	}
+	public int getPoint_y() {
+		return point_y;
+	}
+	public void setPoint_y(int point_y) {
+		this.point_y = point_y;
+	}
+	public double getPoint_fontsize() {
+		return point_fontsize;
+	}
+	public void setPoint_fontsize(double point_fontsize) {
+		this.point_fontsize = point_fontsize;
+	}
+	public String getBuytext_text() {
+		return buytext_text;
+	}
+	public void setBuytext_text(String buytext_text) {
+		this.buytext_text = buytext_text;
+	}
+	public int getBuytext_x() {
+		return buytext_x;
+	}
+	public void setBuytext_x(int buytext_x) {
+		this.buytext_x = buytext_x;
+	}
+	public int getBuytext_y() {
+		return buytext_y;
+	}
+	public void setBuytext_y(int buytext_y) {
+		this.buytext_y = buytext_y;
+	}
+	public double getBuytext_fontsize() {
+		return buytext_fontsize;
+	}
+	public void setBuytext_fontsize(double buytext_fontsize) {
+		this.buytext_fontsize = buytext_fontsize;
+	}
+	public int getInput_x() {
+		return input_x;
+	}
+	public void setInput_x(int input_x) {
+		this.input_x = input_x;
+	}
+	public int getInput_y() {
+		return input_y;
+	}
+	public void setInput_y(int input_y) {
+		this.input_y = input_y;
+	}
+	public int getInput_w() {
+		return input_w;
+	}
+	public void setInput_w(int input_w) {
+		this.input_w = input_w;
+	}
+	public int getInput_h() {
+		return input_h;
+	}
+	public void setInput_h(int input_h) {
+		this.input_h = input_h;
+	}
+	public String getBack_text() {
+		return back_text;
+	}
+	public void setBack_text(String back_text) {
+		this.back_text = back_text;
+	}
+	public String getBack_image1() {
+		return back_image1;
+	}
+	public void setBack_image1(String back_image1) {
+		this.back_image1 = back_image1;
+	}
+	public String getBack_image2() {
+		return back_image2;
+	}
+	public void setBack_image2(String back_image2) {
+		this.back_image2 = back_image2;
+	}
+	public int getBack_x() {
+		return back_x;
+	}
+	public void setBack_x(int back_x) {
+		this.back_x = back_x;
+	}
+	public int getBack_y() {
+		return back_y;
+	}
+	public void setBack_y(int back_y) {
+		this.back_y = back_y;
+	}
+	public int getBack_w() {
+		return back_w;
+	}
+	public void setBack_w(int back_w) {
+		this.back_w = back_w;
+	}
+	public int getBack_h() {
+		return back_h;
+	}
+	public void setBack_h(int back_h) {
+		this.back_h = back_h;
+	}
+	public String getDown_text() {
+		return down_text;
+	}
+	public void setDown_text(String down_text) {
+		this.down_text = down_text;
+	}
+	public String getDown_image1() {
+		return down_image1;
+	}
+	public void setDown_image1(String down_image1) {
+		this.down_image1 = down_image1;
+	}
+	public String getDown_image2() {
+		return down_image2;
+	}
+	public void setDown_image2(String down_image2) {
+		this.down_image2 = down_image2;
+	}
+	public int getDown_x() {
+		return down_x;
+	}
+	public void setDown_x(int down_x) {
+		this.down_x = down_x;
+	}
+	public int getDown_y() {
+		return down_y;
+	}
+	public void setDown_y(int down_y) {
+		this.down_y = down_y;
+	}
+	public int getDown_w() {
+		return down_w;
+	}
+	public void setDown_w(int down_w) {
+		this.down_w = down_w;
+	}
+	public int getDown_h() {
+		return down_h;
+	}
+	public void setDown_h(int down_h) {
+		this.down_h = down_h;
+	}
+	public String getBuy_text() {
+		return buy_text;
+	}
+	public void setBuy_text(String buy_text) {
+		this.buy_text = buy_text;
+	}
+	public String getBuy_image1() {
+		return buy_image1;
+	}
+	public void setBuy_image1(String buy_image1) {
+		this.buy_image1 = buy_image1;
+	}
+	public String getBuy_image2() {
+		return buy_image2;
+	}
+	public void setBuy_image2(String buy_image2) {
+		this.buy_image2 = buy_image2;
+	}
+	public int getBuy_x() {
+		return buy_x;
+	}
+	public void setBuy_x(int buy_x) {
+		this.buy_x = buy_x;
+	}
+	public int getBuy_y() {
+		return buy_y;
+	}
+	public void setBuy_y(int buy_y) {
+		this.buy_y = buy_y;
+	}
+	public int getBuy_w() {
+		return buy_w;
+	}
+	public void setBuy_w(int buy_w) {
+		this.buy_w = buy_w;
+	}
+	public int getBuy_h() {
+		return buy_h;
+	}
+	public void setBuy_h(int buy_h) {
+		this.buy_h = buy_h;
+	}
+	
+}
